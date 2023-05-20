c310a690cf0c86dcd90cbb70979504d6bc63eb1a
Yousef Hesham <youssef100h@gmail.com>
2023-05-20 13:42:05 +0300

    create messages resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: true }, length: {maximum:15 , minimum: 3}
+    has_many :messages
     has_secure_password
 end
